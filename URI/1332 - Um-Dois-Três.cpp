bfd31300dc49d5b019767a93ada13583bf4922df
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-07-26 12:14:28 -0300

    Create 1332 - Um-Dois-Três.cpp

---
+++ URI/1332 - Um-Dois-Três.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    string p;
+    int one=0 , two=0;
+    cin >> n;
+    cin.ignore();
+    for (int i= 0; i < n; i++){
+        cin >> p;
+
+        if (p.length() == 5){
+            cout << "3" << endl;
+        } else{
+            if (p[0] == 'o')
+                one++;
+            if (p[1] == 'n')
+                one++;
+            if (p[2] == 'e')
+                one++;
+            if (p[0] == 't')
+                two++;
+            if (p[1] == 'w')
+                two++;
+            if (p[2] == 'o')
+                two++;
+
+            if (one > two)
+                cout << "1" << endl;
+             else
+                cout << "2" << endl;
+        }
+        one =0; two = 0;
+    }
+    return 0;
+}
