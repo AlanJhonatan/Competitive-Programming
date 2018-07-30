f5a057ebb25d8b7d4979362089201ca53a29d8af
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-07-30 17:45:05 -0300

    Create 1585 - Making Kites.cpp

---
+++ URI/1585 - Making Kites.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define  bkl  cout<<endl;
+using namespace std;
+int main(){
+  int n;
+  cin >> n;
+  while (n--){
+  	int x,y;
+  	cin >> x >> y;
+  	x = (x * y)/2;
+  	cout << x <<" cm2" << endl;
+  }	
+  return 0;
+}
