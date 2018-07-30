3c3aa02718af612f533fda2c0d7b0e4ef66b8744
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-07-30 18:03:03 -0300

    Create last-code.cpp

---
+++ last-code.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+#define  bkl  cout<<endl;
+using namespace std;
+int main(){
+  int n,t,x;
+  cin >> t;
+  while (t--){
+  	vector <int> v;
+  	int sw = 0,temp;
+  	cin >> n;
+  		while (n--){
+	  		cin >> x;
+	  		v.push_back(x);
+		} 
+	for (unsigned int i = 0; i < v.size(); i++){
+		if(v[i] > v[i+1] && i != static_cast <int> (v.size()) - 1){
+			sw++;
+			temp = v[i];
+			v[i+1] = v[i];
+			v[i] = temp;
+			i = 0;
+		}
+	}
+	cout <<"Optimal train swapping takes " << sw <<" swaps."<<  endl;	  	
+  }
+  return 0;
+}
