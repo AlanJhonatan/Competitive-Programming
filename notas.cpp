1b71e66f8a5c5307292bc9d59501e5980cb33d5a
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-07-31 08:26:08 -0300

    Create notas.cpp

---
+++ notas.cpp
@@ -0,0 +1,37 @@
+/*              -- to sort a vector -- 
+*  sort(numbers.begin(), numbers.end(), greater<int>())
+*                                       |-> greater/less.
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*
+*/
