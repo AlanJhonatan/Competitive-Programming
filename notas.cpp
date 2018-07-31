6a9e4d3829beb207e8253f11484dc751e4054d81
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-07-31 12:18:32 -0300

    Update notas.cpp

--- notas.cpp
+++ notas.cpp
@@ -6,12 +6,21 @@
               *COMPARE
               *RECURSIVA
 */
+// -----------------------------------------------------------------------------------------------------------//
 
-
-//                  -- to sort a vector -- 
+//                -- ORGANIZAR UM VETOR -- 
         sort(numbers.begin(), numbers.end(), greater<int>())
 //                                           |-> greater/less. (greater = decrescente, less = ascendente)
 
+		//      OU TAMBÉM
+		
+	struct greater{
+    		template<class T>
+    		bool operator()(T const &a, T const &b) const { return a > b; }
+	}   ;
+		std::sort(numbers.begin(), numbers.end(), greater());
+
+// -----------------------------------------------------------------------------------------------------------//
 
 //                -- RETORNAR A POSIÇÃO DE UM ELEMENTO EM UM VETOR --
            ptrdiff_t pos = distance(VETOR.begin(), find(VETOR.begin(), VETOR.end(), ELEMENTO));
