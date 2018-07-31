94700da7ce59b5f5805a59f0b27d5c60f3de2b43
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-07-31 10:31:45 -0300

    Update notas.cpp

--- notas.cpp
+++ notas.cpp
@@ -1,37 +1,21 @@
-/*              -- to sort a vector -- 
-*  sort(numbers.begin(), numbers.end(), greater<int>())
-*                                       |-> greater/less. (greater = decrescente, less = ascendente)
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
-*
+/*
+            ESTUDAR SOBRE:
+              *PONTEIROS
+              *FIND
+              *SORT
+              *COMPARE
+              *RECURSIVA
 */
+
+
+//                  -- to sort a vector -- 
+        sort(numbers.begin(), numbers.end(), greater<int>())
+//                                           |-> greater/less. (greater = decrescente, less = ascendente)
+
+
+//                -- RETORNAR A POSIÇÃO DE UM ELEMENTO EM UM VETOR --
+           ptrdiff_t pos = distance(VETOR.begin(), find(VETOR.begin(), VETOR.end(), ELEMENTO));
+ 
+                                 if(pos >= VETOR.size())  
+//              CHECAGEM: SE O ELEMENTO *NÃO FOR ENCONTRADO* A POSIÇÃO É IGUAL AO TAMANHO
+		
