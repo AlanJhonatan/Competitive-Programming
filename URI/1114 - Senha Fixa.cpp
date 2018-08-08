b68527db09f069da684d07f32c8c1f9df9cc8b5c
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-08-08 08:14:34 -0300

    Create  1114 - Senha Fixa.cpp

---
+++ URI/1114 - Senha Fixa.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	int x;
+	while (1) {
+		cin >> x;
+		if (x == 2002){
+			break;
+		}
+		cout << "Senha Invalida" << endl;
+	} 
+	cout << "Acesso Permitido" << endl;
+	
+	return 0;
+}
