332282d11da20c1f82a05b639ed0d508cf43e578
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-08-13 08:20:35 -0300

    Create  1181 - Linha na Matriz.cpp

---
+++ URI/1181 - Linha na Matriz.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	int l;
+	char t;
+	double m[12][12], x;
+	
+	cin >> l;
+	cin.ignore();
+	cin >> t;
+	
+	for (int i =0; i < 12; i++){
+		for (int j =0; j < 12; j++){
+			cin >> x;
+			m[i][j] = x;
+		}
+	}
+	if (t == 'S'){
+		double acum=0;
+		for (int i = 0; i < 12; i++){
+			acum += m[l][i];
+		}
+		cout << fixed << setprecision(1) << acum << endl;
+	} else {
+		double acum = 0;
+		for (int i = 0; i < 12; i++){
+			acum += m[l][i];
+		}
+		acum = acum /12;
+		cout << fixed << setprecision(1) << acum << endl;
+	}
+	return 0;
+}
