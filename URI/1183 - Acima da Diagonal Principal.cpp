eaf7568951ec4266f91058d5ae001877cebfd58c
Alan Jhonatan <alan.amorim.franca@gmail.com>
2018-08-13 08:25:45 -0300

    Create 1183 - Acima da Diagonal Principal.cpp

---
+++ URI/1183 - Acima da Diagonal Principal.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main (){
+	char c;
+	double x,r,count=0;
+	double m[12][12];
+	
+	cin >> c;
+	
+	for (int i =0; i < 12; i++){
+		for (int j = 0; j < 12; j++){
+			cin >> x;
+			m[i][j] = x;
+		}
+	}
+	
+	for (int i =0; i < 12; i++){
+		for (int j = 0; j < 12; j++){
+			if (j > i){// SE TA
+				r += m[i][j];
+				count++;	
+			}
+		}
+	}
+	cout << fixed << setprecision(1);
+	
+	if (c == 'S'){
+		cout << r << endl;
+	} else {
+		cout << r / count << endl;
+	}
+	
+	return 0;
+}
